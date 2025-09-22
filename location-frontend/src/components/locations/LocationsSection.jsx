4faf49987fff8952a84a504e1f262611b715cbd5
Roma <mshkv.roman@gmail.com>
2025-09-21 22:00:55 -0500

    Added scrolling to cards

--- location-frontend/src/components/locations/LocationsSection.jsx
+++ location-frontend/src/components/locations/LocationsSection.jsx
@@ -12,30 +12,34 @@ const LocationsSection = ({
 }) => {
   return (
     <div
-      className={`bg-sunshine w-full overflow-y-auto rounded-lg p-6 text-gray-800 lg:w-1/3 ${className}`}
+      className={`bg-sunshine flex h-[85vh] w-full flex-col rounded-lg p-6 text-gray-800 lg:w-1/3 ${className}`}
     >
+      {/* Header */}
       <h2 className="font-display mb-4 text-center text-3xl font-bold text-[#007fbd]">
         Locations
       </h2>
 
-      {locations.length > 0 ? (
-        locations.map((location) => (
-          <LocationCard
-            key={location._id}
-            location={location}
-            isHovered={hoveredLocationId === location._id}
-            isSelected={selectedLocationId === location._id}
-            setHoveredLocationId={setHoveredLocationId}
-            setSelectedLocationId={setSelectedLocationId}
-            isOpen={isOpen}
-            setIsOpen={setIsOpen}
-          />
-        ))
-      ) : (
-        <p className="text-gray-500">No locations available.</p>
-      )}
+      {/* Scrollable list (only this area scrolls) */}
+      <div className="min-h-0 flex-1 overflow-y-auto">
+        {locations.length > 0 ? (
+          locations.map((location) => (
+            <LocationCard
+              key={location._id}
+              location={location}
+              isHovered={hoveredLocationId === location._id}
+              isSelected={selectedLocationId === location._id}
+              setHoveredLocationId={setHoveredLocationId}
+              setSelectedLocationId={setSelectedLocationId}
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+            />
+          ))
+        ) : (
+          <p className="text-gray-500">No locations available.</p>
+        )}
+      </div>
 
-      {/* Apply Button */}
+      {/* Apply Button (stays visible; list above it scrolls) */}
       <div className="mt-6 flex justify-center">
         <a
           href="https://docs.google.com/forms/d/1TTewQs-5fgsVm9PlEe8-7IRluOuX5l7oBcMRPMMe2xU/edit"
